abd20d84b7cdf2fc280a93eb1d8b566edfcde537
Roger Zea <rogerzea.dm@gmail.com>
2025-03-03 14:18:29 -0500

    El tablero de juego se crear automaticamente con la clase GameBoard, los botones del tablero tienen funcionalidad para marcarse y actualizan los modulos 'last-number' y 'last-number-list'

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
 /**
- * Main JS
-*/
+ * main.js
+ */
+
+import { gameBoard } from './components/game-board.js';
+// import { initializeGameMode } from './components/game-mode.js';
+// import { initializeGameScore } from './components/game-score.js';
+// import { initializeDisplayInfo } from './components/display-info.js';
+
+// Inicializar los componentes
+// gameBoard ya se inicializa al importarse debido a la instanciación en game-board.js
+// initializeGameMode();
+// initializeGameScore();
+// initializeDisplayInfo();
+
+// Lógica principal del juego...
+
+console.log('Aplicación Bingo inicializada.');
