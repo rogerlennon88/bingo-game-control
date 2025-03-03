99408d1465e385b204936641f10333254ad51c0d
Roger Zea <rogerzea.dm@gmail.com>
2025-03-03 15:58:41 -0500

    Mensaje de confirmacion al presionar Seleccionar para inciar el juego. Se habilita el Tablero de Juego

--- js/components/game-board.js
+++ js/components/game-board.js
@@ -3,50 +3,43 @@
  */
 
 class GameBoard {
-  constructor(boardElementId = "grid-game-board") {
+  constructor(boardElementId = 'grid-game-board') {
     this.boardElementId = boardElementId;
     this.markedBalls = new Set();
-    this.ballHistory = []; // Array para almacenar el historial de balotas marcadas
+    this.ballHistory = [];
     this.initializeBoard();
   }
 
   initializeBoard() {
     const board = document.getElementById(this.boardElementId);
-
     if (!board) {
       console.error(`Element with id ${this.boardElementId} not found.`);
       return;
     }
-
     this.renderBoard(board);
-    board.addEventListener("click", this.handleBallClick.bind(this));
+    board.addEventListener('click', this.handleBallClick.bind(this));
   }
 
   renderBoard(board) {
-    const letters = ["B", "I", "N", "G", "O"];
+    const letters = ['B', 'I', 'N', 'G', 'O'];
     let ballNumber = 1;
-
     for (let i = 0; i < 5; i++) {
-      const group = document.createElement("div");
-      group.classList.add("group");
-
-      // Letter cell
-      const letterCell = document.createElement("div");
-      letterCell.classList.add("cell", "letter");
-      const letterButton = document.createElement("button");
+      const group = document.createElement('div');
+      group.classList.add('group');
+      const letterCell = document.createElement('div');
+      letterCell.classList.add('cell', 'letter');
+      const letterButton = document.createElement('button');
       letterButton.id = `${letters[i].toLowerCase()}-ggb`;
-      letterButton.classList.add("btn", "btn-ggb");
+      letterButton.classList.add('btn', 'btn-ggb');
       letterButton.textContent = letters[i];
       letterCell.appendChild(letterButton);
       group.appendChild(letterCell);
-
-      // Number cells
       for (let j = 0; j < 15; j++) {
-        const numberCell = document.createElement("div");
-        numberCell.classList.add("cell", "num");
-        const numberButton = document.createElement("button");
+        const numberCell = document.createElement('div');
+        numberCell.classList.add('cell', 'num');
+        const numberButton = document.createElement('button');
         numberButton.id = ballNumber.toString();
-        numberButton.classList.add("btn", "btn-ggb");
+        numberButton.classList.add('btn', 'btn-ggb');
         numberButton.textContent = ballNumber.toString();
         numberCell.appendChild(numberButton);
         group.appendChild(numberCell);
@@ -57,7 +50,7 @@ class GameBoard {
   }
 
   handleBallClick(event) {
-    if (event.target.classList.contains("btn-ggb")) {
+    if (event.target.classList.contains('btn-ggb')) {
       const ballNumber = parseInt(event.target.id);
       this.markBall(ballNumber);
       this.updateBallHistory(ballNumber);
@@ -70,37 +63,29 @@ class GameBoard {
     if (!this.markedBalls.has(ballNumber)) {
       this.markedBalls.add(ballNumber);
       const button = document.getElementById(ballNumber);
-      button.classList.add("marked");
-      button.disabled = true; // Deshabilitar el botón
-      console.log(`Balota marcada: ${ballNumber}`);
+      button.classList.add('marked');
+      button.disabled = true;
     }
   }
 
   updateBallHistory(ballNumber) {
     this.ballHistory.push(ballNumber);
-    console.log("Historial de balotas:", this.ballHistory);
   }
 
   updateLastBallDisplay(ballNumber) {
-    const lastBallDisplay = document.querySelector(".last-number .num-1");
-    lastBallDisplay.textContent = ballNumber.toString().padStart(2, "0");
+    const lastBallDisplay = document.querySelector('.last-number .num-1');
+    lastBallDisplay.textContent = ballNumber.toString().padStart(2, '0');
   }
 
   updateLastBallsList() {
-    const lastBallsList = document.querySelectorAll(
-      ".last-number-list .num-2, .last-number-list .num-3, .last-number-list .num-4, .last-number-list .num-5"
-    );
-    const history = this.ballHistory.slice(-5, -1).reverse(); // Obtener las últimas 4 balotas
-
+    const lastBallsList = document.querySelectorAll('.last-number-list .num-2, .last-number-list .num-3, .last-number-list .num-4, .last-number-list .num-5');
+    const history = this.ballHistory.slice(-5, -1).reverse();
     lastBallsList.forEach((item, index) => {
-      item.textContent = history[index]
-        ? history[index].toString().padStart(2, "0")
-        : "00";
+      item.textContent = history[index] ? history[index].toString().padStart(2, '0') : '00';
     });
   }
 }
 
-// Inicializar el tablero de juego
 const gameBoard = new GameBoard();
 
 export { gameBoard };
