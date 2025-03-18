41b981547432ca998a4dd0f3119e0110fd445b5d
Roger Zea <rogerzea.dm@gmail.com>
2025-03-18 08:59:25 -0500

    Configuracion basica de NodeJS con Express. Se crea un gitignore

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const port = 3000;
+
+// Servir archivos estáticos desde el directorio raíz
+app.use(express.static(path.join(__dirname, ".")));
+
+// Ruta principal para servir index.html
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.listen(port, () => {
+  console.log(`Servidor escuchando en http://localhost:${port}`);
+});
